13b5c825d0bd7a67010acdbb9234c9676e3b6a50
Srishti Sehtia <srishti0123@gmail.com>
2018-02-22 14:13:04 -0800

    set up the creatures create action

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -5,4 +5,12 @@ class CreaturesController < ApplicationController
 
   def new_creature
   end
+
+  def create
+    creature_params = params.require(:creature).permit(:name, :description)
+    creature = Creature.new(creature_params)
+    if creature.save
+      redirect_to creatures_path
+    end
+  end
 end
